fdeb5ad2d076c98fb570e7658d057f21d4d28ee5
Daria-Nekrashevich <daria.nkrshvch@gmail.com>
2024-03-17 22:06:36 +0300

    Add page with artists and page with biography of artists

--- src/main/java/nekrashevich/artgallery/repository/ArtistRepository.java
+++ src/main/java/nekrashevich/artgallery/repository/ArtistRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
+
 @Repository
 public interface ArtistRepository extends JpaRepository<Artist, UUID> {
+    Artist findArtistById(UUID id);
 }
